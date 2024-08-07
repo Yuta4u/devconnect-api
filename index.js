317fab979773f2b4f0114b2290b4cf9558fa7ebf
Yuta4u <yudhistira4u@gmail.com>
2024-08-08 05:45:05 +0700

    lupa buat verceljsonnya coyy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.options("*", cors())
 
 app.use("/api/dev", devRoutes)
 
-const PORT = 5000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
